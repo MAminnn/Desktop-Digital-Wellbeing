974d6f66ff315f57957bb79c7cc4154a8664bdd0
Amin <aminkarvizi1384@gmail.com>
2023-05-14 10:19:58 +0330

    Enable logging for windows service

--- src/Services/DesktopDigitalWellbeing/WorkerService/Program.cs
+++ src/Services/DesktopDigitalWellbeing/WorkerService/Program.cs
@@ -1,10 +1,29 @@
+using Serilog;
 using WorkerService;
 
+Log.Logger = new LoggerConfiguration().MinimumLevel.Debug()
+    .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.File(@"Z:\log.txt")
+    .CreateLogger();
+
 IHost host = Host.CreateDefaultBuilder(args)
+    .UseWindowsService()
     .ConfigureServices(services =>
     {
         services.AddHostedService<Worker>();
-    })
+    }).UseSerilog()
     .Build();
 
-host.Run();
+try
+{
+    Log.Information("Starting the service ...");
+    host.Run();
+    Log.Information("Service stopped");
+    return;
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "There was problem starting the service");
+    return;
+}
